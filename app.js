e8a7e7b311a8111e8cbedd2530f58599b2961046
bjwine0 <bradsmilek@gmail.com>
2019-08-17 22:03:07 -0700

    update readme/deployed to AWS

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 
 const io = require('socket.io-client');
 
-const socket = io.connect('http://172.16.0.231:3000');
+const socket = io.connect('http://SocketServer-env.2ixrzi57cy.us-east-2.elasticbeanstalk.com');
 
 const faker = require('faker');
 
+//emits speak and passes a hacker phrase as payload every second
 setInterval( () => {
   socket.emit('speak', faker.hacker.phrase());
 },1000);
--- console.js
+++ console.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const theword = require('theword.js');
+const theword = require('./theword.js');
 
 const io = require('socket.io-client');
+//connecting to AWS
+const socket = io.connect('http://SocketServer-env.2ixrzi57cy.us-east-2.elasticbeanstalk.com');
 
-const socket = io.connect('http://172.16.0.231:3000');
 
+//listenting for a string
 socket.on('the-bird', theword);
